db025af87ef02f8e7058f5b8ff326a289516175b
DevopsTraining <51361708+splunklearners@users.noreply.github.com>
2019-11-13 17:03:26 +0530

    Update TestBase.java

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -41,7 +41,7 @@ public class TestBase {
 		String browserName = prop.getProperty("browser");
 		
 		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", "c:\test\chromedriver");	
+			System.setProperty("webdriver.chrome.driver", "/test/chromedriver");	
 			driver = new ChromeDriver(); 
 		}
 		else if(browserName.equals("FF")){
